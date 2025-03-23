17906aeec17f54914fccf2e9e164f88d6857bac6
Diana <karmadonowa.d@yandex.ru>
2025-03-23 22:40:31 +0800

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->randomElement([
+                'Kultlab Pineapple Isotonic',
+                'Kultlab Whey Protein',
+                'Батончик Chikabar в белом шоколаде',
+                'Fresh Iron'
+            ]),
+            'price' => $this->faker->randomFloat(0, 75, 1400),
+            'description' => $this->faker->paragraph(),
+            'image' => $this->faker->imageUrl(400, 400, 'products'),
+            'category' => $this->faker->randomElement([
+                'Протеиновые батончики и печенье',
+                'Протеины',
+                'Витамины и минералы',
+                'Изотоники'
+            ])
+        ];
+    }
+}
