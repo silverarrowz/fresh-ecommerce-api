89ce210d07dc3692f8e29cebaa3dd3b71587dc4f
Diana <karmadonowa.d@yandex.ru>
2025-04-01 16:19:25 +0800

    .

--- database/migrations/2025_04_01_080458_add_category_id_to_products_table.php
+++ database/migrations/2025_04_01_080458_add_category_id_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('category');
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('cascade');
         });
     }
 
